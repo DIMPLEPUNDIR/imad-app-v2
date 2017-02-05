60163907fdb9fec4869609dafad172545ff8afb2
DIMPLEPUNDIR <pundirdimple@gmail.com>
2017-02-05 21:26:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,12 +13,12 @@ app.get('/article-one',function (req,res) {
   res.send('Article one requested and will be served here'); 
 });
 
-app.get('/article-two',function (req,res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-two.html); 
+app.get('/article-two', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'article two.html'));
 });
 
 app.get('/article-three',function (req,res) {
-  res.send('Article three requested and will be served here'); 
+  res.send('Article one requested and will be served here'); 
 });
 
 app.get('/ui/style.css', function (req, res) {
